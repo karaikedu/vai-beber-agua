fe33e32cb95965808b131d152f3ca6c4f7c79638
karaikedu <dkell..silv4@gmail.com>
2024-05-07 08:43:12 -0300

    vizualização da imagem agora via janela tkinter e não pelo vizualizador de imagem do sistema

--- vai_beber_agua.py
+++ vai_beber_agua.py
@@ -4,9 +4,29 @@ Xx VAI BEBER ÁGUA!! xX
 
 Lembrete pra ir beber água a cada 3hrs mais ou menos :D
 """
+import tkinter
 import random
 import time
 import os
+
+
+def mostrar_imagem(path_imagem:str):
+    """
+    Cria uma janela tkinter fullscreen para mostrar a imagens preenchida na tela
+    Só clicar na tela que a janela é destruída
+    """
+    root = tkinter.Tk()
+    img = tkinter.PhotoImage(master=root, file=path_imagem)
+    label = tkinter.Label(master=root, image=img)
+    label.place(
+        x=0,
+        y=0,
+        width=root.winfo_screenwidth(),
+        height=root.winfo_screenheight()
+    )
+    root.bind('<Button-1>', lambda event=None: root.destroy())
+    root.attributes('-fullscreen', 1)
+    root.mainloop()
 
 
 def msg_aguardando(motivo='\b', tempo=10):
@@ -26,14 +46,15 @@ def msg_aguardando(motivo='\b', tempo=10):
 
 
 PATH_IMAGENS = os.path.join('bisaco', 'imagens', '')
-ALBUM = [img for img in os.listdir(PATH_IMAGENS) if img.endswith('.jpg')]
+ALBUM = [img for img in os.listdir(PATH_IMAGENS) if img.endswith('.png')]
 
 TEMPO_ESPERA = 10800
 
 while True:
     random.shuffle(ALBUM)
-    for indice, img in enumerate(ALBUM, start=1):
-        msg_aguardando(motivo='um tempin', tempo=TEMPO_ESPERA)
+
+    for indice, imagem in enumerate(ALBUM, start=1):
+        msg_aguardando(motivo='um pokin', tempo=TEMPO_ESPERA)
         print(f'[{indice}] VAI BEBER ÁGUA!!! :D')
-        os.system(f'start /max {PATH_IMAGENS}"{img}"')
-        kuteis.divisor_animado(colunas=30)
+        mostrar_imagem(path_imagem=PATH_IMAGENS + imagem)
+        print('-' * 30)
